9fdb8561a22624926b0d23ae8f3654ec36e72089
johngerome <johngerome@gmail.com>
2022-08-23 21:05:55 +0800

    chore: add health check endpoint

--- apps/api/start/routes.ts
+++ apps/api/start/routes.ts
@@ -18,6 +18,7 @@
 |
 */
 
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', ({ view }) => {
@@ -80,3 +81,9 @@ Route.group(() => {
 })
   .prefix('/admin')
   .middleware('auth')
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy ? response.ok(report) : response.badRequest(report)
+})
