2dad443feec65ef59b52d5ca56139a6a25bd8022
John Gerome Baldonado <johngerome@gmail.com>
2022-08-26 19:35:28 +0800

    chore: setup redis (#33)

--- apps/api/env.ts
+++ apps/api/env.ts
@@ -21,4 +21,8 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  REDIS_CONNECTION: Env.schema.enum(['local'] as const),
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
 })
