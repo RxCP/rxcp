64210d85c59d3796471bfd348f18073cee22e9b8
John Gerome Baldonado <johngerome@gmail.com>
2022-09-10 17:42:33 +0800

    feat(web): add forgot password page (#60)

---
+++ apps/admin/src/components/organisms/forms/ForgotPassword.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Button from '@components/atoms/buttons/Button.svelte';
+  import FormInput from '@components/atoms/forms/FormInput.svelte';
+  import FormLabel from '@components/atoms/forms/FormLabel.svelte';
+</script>
+
+<form>
+  <div class="mb-6 space-y-2">
+    <FormLabel htmlFor="email" text="Email address" />
+    <FormInput id="email" type="email" />
+  </div>
+  <Button type="submit">Reset password</Button>
+</form>
