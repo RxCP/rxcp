5cdbf4220413f948a9458a8cc8221e46db007e4d
John Gerome Baldonado <johngerome@gmail.com>
2022-09-14 21:19:09 +0800

    feat(web): add theme switcher (#67)

---
+++ apps/admin/src/components/atoms/buttons/ButtonToggleTheme.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let className = '';
+
+  let isDarkTheme = false;
+
+  function handleButtonClick() {
+    const html = document.querySelector('html');
+    isDarkTheme = html.classList.contains('dark');
+
+    if (isDarkTheme) {
+      html.classList.remove('dark');
+      return;
+    }
+
+    html.classList.add('dark');
+  }
+</script>
+
+<button
+  type="button"
+  class={`${className} bg-transparent`}
+  on:click={handleButtonClick}
+>
+  <div
+    class:i-tabler-sun={!isDarkTheme}
+    class:i-tabler-moon={isDarkTheme}
+    class="text-3xl"
+  />
+</button>
