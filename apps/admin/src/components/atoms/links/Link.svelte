65403e590a44d49bda916067d475547a0a763a27
John Gerome Baldonado <johngerome@gmail.com>
2022-09-15 18:08:02 +0800

    feat(web): fix responsive layout for login (#69)

--- apps/admin/src/components/atoms/links/Link.svelte
+++ apps/admin/src/components/atoms/links/Link.svelte
@@ -1,7 +1,8 @@
-<script>
-  export let to;
+<script lang="ts">
+  export let to: string;
+  export let className: string = '';
 </script>
 
-<a href={to} class="text-sm font-medium text-blue-500">
+<a href={to} class="text-sm font-medium text-blue-500 {className}">
   <slot />
 </a>
