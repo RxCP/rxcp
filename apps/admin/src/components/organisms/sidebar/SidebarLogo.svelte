857b3d510e209b08d6526c7bd9aa277fdc031c0c
johngerome <johngerome@gmail.com>
2022-09-16 21:28:44 +0800

    style: remove top spacing on logo

--- apps/admin/src/components/organisms/sidebar/SidebarLogo.svelte
+++ apps/admin/src/components/organisms/sidebar/SidebarLogo.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="ml-4 lg:ml-0 xl:ml-4 pt-4 pb-8 text-center flex items-center space-x-4 font-medium"
+  class="ml-4 lg:ml-0 xl:ml-4 pb-8 text-center flex items-center space-x-4 font-medium"
 >
   <PrimaryLogo />
   <span class="text-2xl font-bold lg:hidden xl:inline-block">RXCP</span>
