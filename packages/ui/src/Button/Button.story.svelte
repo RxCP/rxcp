f556e146a0030a3641f9c198e97e75ac7056a392
John Gerome Baldonado <johngerome@gmail.com>
2022-09-17 20:56:58 +0800

    style(web): add more variations for button (#78)

--- packages/ui/src/Button/Button.story.svelte
+++ packages/ui/src/Button/Button.story.svelte
@@ -15,41 +15,70 @@
     eventCallback,
   } from '@vitebook/client/addons';
   import Button from './Button.svelte';
+  import type { buttonColor } from './ButtonTypes';
 
-  let text : string = 'Primary Button';
-  let disabled : boolean = false;
-  let isLoading : boolean = false;
+  let text: string = 'Button';
+  let color: buttonColor = 'primary';
+  let isDisabled: boolean = false;
+  let isLoading: boolean = false;
 </script>
 
-<Variant name="Default" description="The default button.">
-  <Button {disabled} {isLoading} on:click={eventCallback}>{text}</Button>
+<Variant name="Solid" description="The solid button.">
+  <Button {color} {isDisabled} {isLoading} on:click={eventCallback}>
+    {text}
+  </Button>
+</Variant>
+
+<Variant name="Ghost" description="The ghost button.">
+  <Button
+    variant="ghost"
+    {color}
+    {isDisabled}
+    {isLoading}
+    on:click={eventCallback}
+  >
+    {text}
+  </Button>
 </Variant>
 
 <Variant
   name="Disabled"
-  description="The disabled button."
   on:enter={() => {
-    disabled = true;
+    isDisabled = true;
   }}
   on:exit={() => {
-    disabled = false;
+    isDisabled = false;
   }}
 >
-  <Button {disabled} {isLoading}>{text}</Button>
+  <Button {color} {isDisabled} {isLoading}>{text}</Button>
+</Variant>
+
+<Variant name="Sizes">
+  <Button {color} {isDisabled} {isLoading} size="xs">{text}</Button>
+  <Button {color} {isDisabled} {isLoading} size="sm">{text}</Button>
+  <Button {color} {isDisabled} {isLoading} size="md">{text}</Button>
+  <Button {color} {isDisabled} {isLoading} size="lg">{text}</Button>
 </Variant>
 
 <ControlsAddon>
-<div class="vbk-controls">
-  <label>
-    text <input type="text" bind:value={text} />
-  </label>
-  <label>
-    Disabled <input type="checkbox" bind:checked={disabled} />
-  </label>
-  <label>
-    isLoading <input type="checkbox" bind:checked={isLoading} />
-  </label>
-</div>
+  <div class="vbk-controls">
+    <label>
+      text <input type="text" bind:value={text} />
+    </label>
+    <label>
+      Color
+      <select bind:value={color}>
+        <option value="primary">Primary</option>
+        <option value="secondary">Secondary</option>
+      </select>
+    </label>
+    <label>
+      Disabled <input type="checkbox" bind:checked={isDisabled} />
+    </label>
+    <label>
+      isLoading <input type="checkbox" bind:checked={isLoading} />
+    </label>
+  </div>
 </ControlsAddon>
 
 <EventsAddon />
