5b34243ff8ceb16830f68310c2e83b124343601f
johngerome <johngerome@gmail.com>
2022-09-25 12:54:06 +0800

    feat(web): refactor currentPath and sidebar menu items

---
+++ apps/admin/src/components/organisms/profile/ProfileSidebarMenu.svelte
@@ -0,0 +1,32 @@
+<script>
+  import MainSidebarItemLink from '@components/organisms/main/MainSidebarItemLink.svelte';
+  import MainSidebarItemTitle from '@components/organisms/main/MainSidebarItemTitle.svelte';
+  import MainSidebarMenu from '@components/organisms/main/MainSidebarMenu.svelte';
+  import { currentPath } from '@store/url';
+
+  let menu = [
+    {
+      url: '/admin/profile',
+      text: 'Information',
+    },
+    {
+      url: '/admin/profile/change-email',
+      text: 'Change Email',
+    },
+    {
+      url: '/admin/profile/change-password',
+      text: 'Change Password',
+    },
+  ];
+</script>
+
+<MainSidebarMenu>
+  <MainSidebarItemTitle text="Profile" />
+  {#each menu as link}
+    <MainSidebarItemLink
+      to={link.url}
+      text={link.text}
+      isActive={link.url === $currentPath}
+    />
+  {/each}
+</MainSidebarMenu>
