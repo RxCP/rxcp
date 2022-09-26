f35587895c7d6b4193ee60a6ef8a2ba002ff4d4c
johngerome <johngerome@gmail.com>
2022-09-26 19:36:33 +0800

    style(web): refactor forgot pass form and user navbar

--- apps/admin/src/components/molecules/userNav/UserInfo.svelte
+++ apps/admin/src/components/molecules/userNav/UserInfo.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <div un-text="gray-900 dark:white" class="py-3 px-4 text-sm">
-  <div>{name}</div>
-  <div class="font-medium truncate">{email}</div>
+  <div class="line-clamp-1 font-medium" title={name}>{name}</div>
+  <div class="text-sm truncate text-slate-600 dark:text-slate-300">{email}</div>
 </div>
