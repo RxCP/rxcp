aebc45dddef4d389cd7ac6da9cc8c52d826c4c2e
johngerome <johngerome@gmail.com>
2022-09-28 21:56:55 +0800

    chore: update ui docs title

--- packages/rxcp-ui/vite.config.js
+++ packages/rxcp-ui/vite.config.js
@@ -1,16 +1,16 @@
 /// <reference types="histoire" />
 
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { HstSvelte } from '@histoire/plugin-svelte';
-import { defaultColors } from 'histoire';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { HstSvelte } from "@histoire/plugin-svelte";
+import { defaultColors } from "histoire";
 
-import Unocss from '@unocss/vite';
-import { extractorSvelte } from '@unocss/core';
-import presetWebFonts from '@unocss/preset-web-fonts';
-import presetAttributify from '@unocss/preset-attributify';
-import presetIcons from '@unocss/preset-icons';
-import presetUno from '@unocss/preset-uno';
+import Unocss from "@unocss/vite";
+import { extractorSvelte } from "@unocss/core";
+import presetWebFonts from "@unocss/preset-web-fonts";
+import presetAttributify from "@unocss/preset-attributify";
+import presetIcons from "@unocss/preset-icons";
+import presetUno from "@unocss/preset-uno";
 
 const config = {
   // export default defineConfig({
@@ -23,23 +23,23 @@ const config = {
         presetUno(),
         presetIcons(),
         presetWebFonts({
-          provider: 'google',
+          provider: "google",
           fonts: {
             sans: [
               {
-                name: 'Inter',
-                weights: ['400', '500', '700'],
+                name: "Inter",
+                weights: ["400", "500", "700"],
                 italic: true,
               },
               {
-                name: 'sans-serif',
-                provider: 'none',
+                name: "sans-serif",
+                provider: "none",
               },
             ],
           },
         }),
         presetAttributify({
-          prefix: 'un-',
+          prefix: "un-",
           prefixedOnly: true,
         }),
       ],
@@ -47,24 +47,24 @@ const config = {
   ],
   histoire: {
     plugins: [HstSvelte()],
-    setupFile: '/histoire.setup.js',
+    setupFile: "/histoire.setup.js",
     tree: {
       groups: [
         {
-          id: 'top',
-          title: '',
+          id: "top",
+          title: "",
         },
       ],
     },
     theme: {
-      title: 'Oneezy',
+      title: "RxCP UI",
       logo: {
-        square: '/assets/logo.svg',
-        light: '/assets/logo.svg',
-        dark: '/assets/logo.svg',
+        square: "/assets/logo.svg",
+        light: "/assets/logo.svg",
+        dark: "/assets/logo.svg",
       },
-      logoHref: '/',
-      favicon: './assets/favicon.png',
+      logoHref: "/",
+      favicon: "./assets/favicon.png",
 
       colors: {
         gray: defaultColors.gray,
