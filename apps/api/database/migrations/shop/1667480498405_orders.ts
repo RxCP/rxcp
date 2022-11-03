1e4fe513d5d96e18690830aeb0f9793473ed130b
johngerome <johngerome@gmail.com>
2022-11-03 21:15:52 +0800

    feat: setup order

---
+++ apps/api/database/migrations/shop/1667480498405_orders.ts
@@ -0,0 +1,31 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'orders'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.json('customer')
+      table.json('cart')
+      table.json('payments')
+      table.string('payment_status', 10).notNullable()
+      table.integer('total').notNullable()
+      table.integer('subtotal').notNullable()
+      table.integer('paid_total').notNullable()
+      table.integer('discount_total').nullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('canceled_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
