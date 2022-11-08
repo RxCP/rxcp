2307f8902d6ef1fe6775a70928cd01e2ce46c047
johngerome <johngerome@gmail.com>
2022-11-08 21:09:27 +0800

    refactor: show DOM after data is ready

--- apps/admin/src/components/server/status/status-list.svelte
+++ apps/admin/src/components/server/status/status-list.svelte
@@ -2,12 +2,14 @@
   import { onMount } from "svelte";
   import socket from "../../../socket/socket";
 
+  let isLoading = true
   let loginStatus = 'offline'
   let charStatus = 'offline'
   let mapStatus = 'offline'
 
   onMount(() => {
     socket.on('server-status', (data) => {
+      isLoading = false
       loginStatus = data?.login?.status
       charStatus = data?.char?.status
       mapStatus = data?.map?.status
@@ -17,52 +19,55 @@
 
 
 <h2 class="font-bold">Server Status</h2>
-<ul class="mt-4 space-y-2">
-  <li
-    class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
-  >
-    Login
-    <span
-      un-rounded="full"
-      un-text="xs white"
-      class="uppercase px-2 py-1 {loginStatus === 'online' ? 'bg-green-700':'bg-red-500'}"
+{#if !isLoading }
+  <ul class="mt-4 space-y-2">
+    <li
+      class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
     >
-      {loginStatus}
-    </span>
-  </li>
-  <li
-    class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
-  >
-    Character
-    <span
-      un-rounded="full"
-      un-text="xs white"
-      class="uppercase px-2 py-1 {charStatus === 'online' ? 'bg-green-700':'bg-red-500'}"
+      Login
+      <span
+        un-rounded="full"
+        un-text="xs white"
+        class="uppercase px-2 py-1 {loginStatus === 'online' ? 'bg-green-700':'bg-red-500'}"
+      >
+        {loginStatus}
+      </span>
+    </li>
+    <li
+      class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
     >
-      {charStatus}
-    </span>
-  </li>
-  <li
-    class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
-  >
-    Map
-    <span
-      un-rounded="full"
-      un-text="xs white"
-      class="uppercase px-2 py-1 {mapStatus === 'online' ? 'bg-green-700':'bg-red-500'}"
+      Character
+      <span
+        un-rounded="full"
+        un-text="xs white"
+        class="uppercase px-2 py-1 {charStatus === 'online' ? 'bg-green-700':'bg-red-500'}"
+      >
+        {charStatus}
+      </span>
+    </li>
+    <li
+      class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
     >
-      {mapStatus}
-    </span>
-  </li>
-  <li
-    class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
-  >
-    Players Online
-    <span
-      un-rounded="full"
-      class="uppercase text-xs px-2 py-1 bg-slate-600 text-white"
+      Map
+      <span
+        un-rounded="full"
+        un-text="xs white"
+        class="uppercase px-2 py-1 {mapStatus === 'online' ? 'bg-green-700':'bg-red-500'}"
+      >
+        {mapStatus}
+      </span>
+    </li>
+    <li
+      class="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400"
     >
-      100
-    </span>
-  </li>
-</ul>
+      Players Online
+      <span
+        un-rounded="full"
+        class="uppercase text-xs px-2 py-1 bg-slate-600 text-white"
+      >
+        100
+      </span>
+    </li>
+  </ul>
+{/if}
+
